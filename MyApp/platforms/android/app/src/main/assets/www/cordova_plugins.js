eb8f9d67bc4fef3f4033b9e6f8eec83ff9b89aaf
minfrog9907 <jimmy990791@gmail.com>
2018-03-22 15:46:45 +0900

    no message

--- MyApp/platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ MyApp/platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -7,13 +7,63 @@ module.exports = [
     "clobbers": [
       "cordova.plugins.barcodeScanner"
     ]
+  },
+  {
+    "id": "cordova-plugin-dialogs.notification",
+    "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+    "pluginId": "cordova-plugin-dialogs",
+    "merges": [
+      "navigator.notification"
+    ]
+  },
+  {
+    "id": "cordova-plugin-dialogs.notification_android",
+    "file": "plugins/cordova-plugin-dialogs/www/android/notification.js",
+    "pluginId": "cordova-plugin-dialogs",
+    "merges": [
+      "navigator.notification"
+    ]
+  },
+  {
+    "id": "cordova-plugin-camera.Camera",
+    "file": "plugins/cordova-plugin-camera/www/CameraConstants.js",
+    "pluginId": "cordova-plugin-camera",
+    "clobbers": [
+      "Camera"
+    ]
+  },
+  {
+    "id": "cordova-plugin-camera.CameraPopoverOptions",
+    "file": "plugins/cordova-plugin-camera/www/CameraPopoverOptions.js",
+    "pluginId": "cordova-plugin-camera",
+    "clobbers": [
+      "CameraPopoverOptions"
+    ]
+  },
+  {
+    "id": "cordova-plugin-camera.camera",
+    "file": "plugins/cordova-plugin-camera/www/Camera.js",
+    "pluginId": "cordova-plugin-camera",
+    "clobbers": [
+      "navigator.camera"
+    ]
+  },
+  {
+    "id": "cordova-plugin-camera.CameraPopoverHandle",
+    "file": "plugins/cordova-plugin-camera/www/CameraPopoverHandle.js",
+    "pluginId": "cordova-plugin-camera",
+    "clobbers": [
+      "CameraPopoverHandle"
+    ]
   }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
   "cordova-plugin-whitelist": "1.3.3",
-  "phonegap-plugin-barcodescanner": "7.1.0"
+  "phonegap-plugin-barcodescanner": "7.1.0",
+  "cordova-plugin-dialogs": "2.0.1",
+  "cordova-plugin-camera": "4.0.2"
 };
 // BOTTOM OF METADATA
 });
--- MyApp/platforms/android/app/src/main/assets/www/js/index.js
+++ MyApp/platforms/android/app/src/main/assets/www/js/index.js
@@ -16,6 +16,43 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+function scanBarCode(){
+    alert("asdf");
+    cordova.plugins.barcodeScanner.scan(
+        function (result) {
+            alert("We got a barcode\n" +
+                  "Result: " + result.text + "\n" +
+                  "Format: " + result.format + "\n" +
+                  "Cancelled: " + result.cancelled);
+        },
+        function (error) {
+            alert("Scanning failed: " + error);
+        },
+        {
+            preferFrontCamera : false, // 전후면 카메라 (Android, iOS)
+            showFlipCameraButton : true, // 카메라 전환 (Android, iOS)
+            showTorchButton : true, // 플레시 버튼 (Android, iOS)
+            torchOn: true, // 플레시 켜짐 (Android)
+            saveHistory: true, // 스켄 히스토리 (Android) (default false)
+            prompt : "Place a barcode inside the scan area", // 메세지 (Android)
+            resultDisplayDuration: 500, // (Android), display scanned text for X ms. 0 suppresses it entirely, default 1500
+            formats : "PDF_417,EAN_8,EAN_13,QR_CODE", // default: all but PDF_417 and RSS_EXPANDED
+            orientation : "portrait", // 장치와 함께 회전하도록 기본값이 설정 해제 됨 (Android only) (portrait|landscape)
+            disableAnimations : true, // (iOS)
+            disableSuccessBeep: false // (iOS, Android)
+        }
+    );
+}
+
+function testNoti(){
+    navigator.notification.alert(
+        'You are the winner!',  // message
+        null,         // callback
+        'Game Over',            // title
+        'Done'                  // buttonName
+    );
+}
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -44,3 +81,6 @@ var app = {
 };
 
 app.initialize();
+
+document.getElementById("barcode").addEventListener("click", scanBarCode);
+document.getElementById("notification").addEventListener("click", testNoti);
